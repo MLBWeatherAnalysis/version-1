068d1d0ef35ecc64a57f89358db6e4fbd5451825
Brian Ozawa Burns <BrianOzburns@gmail.com>
2022-08-18 16:56:01 -0700

    cin - was

--- openweatherprac.py
+++ openweatherprac.py
@@ -2,6 +2,7 @@ import requests
 import json
 import pandas as pd
 import csv
+import urllib
 
 city = ["ARI", "ATL", "BAL", "BOS", "CHC", "CHW", "CIN", "CLE", "COL", "DET", 
         "HOU", "KCR", "LAA", "LAD", "MIA", "MIL", "MIN", "NYM", "NYY", "OAK", 
@@ -40,6 +41,7 @@ coords = {
     "WAS": [38.872778, -77.0075]
 }
 
+''' -- IGNORE -- '''
 # for each team, get weather data and put in a file
 # for i in range(30):
 #     # different api keys
@@ -69,27 +71,38 @@ coords = {
 #             data = json.loads(response.text)
 
 #             json.dump(data, outfile)
-for i in range(6, 30):
-    # different api keys
-    # "a222d5de136648b180090abd8642fb3f"
-    # "b452f10956ec4907bfcfaf1eeb02079c"
-    #
-    #
-    #
-    api_key = "a222d5de136648b180090abd8642fb3f"
-    lat = coords[city[i]][0]
-    lon = coords[city[i]][1]
-    start_date = "2021-04-01"
-    end_date = "2021-10-04"
-    url = "https://api.weatherbit.io/v2.0/history/hourly?lat=%s&lon=%s&start_date=%s&end_date=%s&tz=local&key=%s&units=I" % (lat, lon, start_date, end_date, api_key)
-    response = requests.get(url)
-    data = json.loads(response.text)
+''' -- -- '''
 
-    with open(city[i] + ".json", "w") as outfile:
-        json.dump(data, outfile)
+# for i in range(30):
+#     # different api keys
+#     # "a222d5de136648b180090abd8642fb3f"
+#     # "b452f10956ec4907bfcfaf1eeb02079c"
+#     # "3acefb75cc7a434e932d4bc8b488ed0a"
+#     # "b6ceebc5363342c6919a45d80b83f9ba"
+#     #
+#     api_key = "b6ceebc5363342c6919a45d80b83f9ba"
+#     lat = coords[city[i]][0]
+#     lon = coords[city[i]][1]
+#     start_date = "2021-04-01"
+#     end_date = "2021-10-04"
+#     url = "https://api.weatherbit.io/v2.0/history/hourly?lat=%s&lon=%s&start_date=%s&end_date=%s&tz=local&key=%s&units=I" % (lat, lon, start_date, end_date, api_key)
+#     response = requests.get(url)
+#     if response == None or response == '':
+#         print("Null or empty string value for data in file")
+#     else:
+#         data = json.loads(response.text)
+#     with open(city[i] + ".json", "w") as outfile:
+#         json.dump(data, outfile)
+#     # response = urllib.request.urlopen(url)
+#     # data = response.read()
+#     # js = json.loads(data.decode("utf-8"))
+    
+#     # with open(city[i] + ".json", "w") as outfile:
+#     #     json.dump(js, outfile)
 
-# for each game, get the appropriate weather data and put in final file
-'''
+# # for each game, get the appropriate weather data and put in final file
+
+
 games = 'filename.csv'
 df = pd.read_csv(games)
 
@@ -124,8 +137,8 @@ for index, game_day in df.iterrows():
     f.close()
 
 data_file.close()
-'''
-    # open file associated with home_team
-    # find appropriate date and time (whatever we end up choosing) in that file
-    # pull temp, rain, and wind
-    # put in final data file
+
+# open file associated with home_team
+# find appropriate date and time (whatever we end up choosing) in that file
+# pull temp, rain, and wind
+# put in final data file
